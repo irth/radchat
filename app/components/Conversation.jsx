d10939d00afdcfb05a089e83ddc667211084a56b
Wojciech Kwolek <me@irth.pl>
2017-07-08 23:52:28 +0200

    don't send the message if it's empty

--- app/components/Conversation.jsx
+++ app/components/Conversation.jsx
@@ -55,7 +55,7 @@ export default class Conversation extends React.Component {
         <ChatInput
           onSubmit={(msg) => {
             if (this.messages != null) this.messages.wrappedInstance.enableAutoScroll();
-            this.props.state.sendMessage(this.props.state.activeChat, msg);
+            if (msg.trim().length > 0) { this.props.state.sendMessage(this.props.state.activeChat, msg); }
           }}
           onChange={msg => this.props.state.setInput(this.props.state.activeChat, msg)}
         />
